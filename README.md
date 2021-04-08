bfc6feb0bf796b2abf31bd1202f6abef3fafdffe
Devin <devin@MacBook-Pro.M1>
2021-04-08 14:48:57 +0700

    Update Flutter Packages

---
+++ README.md
@@ -0,0 +1,5 @@
+# storage_benchmark
+## Getting Started
+
+This project is a benchmarking all flutter packages for storing  key value
+
---
+++ android/app/src/main/kotlin/com/storage_benchmark/app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.storage_benchmark.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
